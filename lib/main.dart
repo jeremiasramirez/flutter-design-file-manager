53ca265cef791bd970e1272e712adb5efed2a273
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-01 20:30:50 -0400

    limpiado comentarios + agregado libreria animaciones

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(StorageManager());
+}
+
+class StorageManager extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: Text('home')
+      )
+    );
+  }
+}
